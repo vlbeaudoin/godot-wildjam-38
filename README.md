5511821e6c05b85b0d8fe6fa0111f70d1f6e8a86
Victor Lacasse-Beaudoin <vlbeaudoin@agecem.org>
2021-10-10 03:55:06 -0400

    Commit initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# godot-wildjam-38
+
+Project for the Godot Wild Jam #38 by https://vlbeaudoin.itch.io/
+
+## Theme
+
+Solarpunk
---
+++ menu/btn_level0.gd
@@ -0,0 +1,24 @@
+extends Button
+
+## VARS
+export(String) var level_name = "level0"
+
+
+## FUNCS
+
+
+## SIGNALS
+func on_button_down():
+	var err = get_tree().change_scene("res://levels/{level}.tscn".format({"level":level_name}))
+	
+	if err != OK:
+		print("[ERR] Could not change scene. (Error code: %s)" % err)
+
+
+## SETGET
+
+
+## EXECUTION
+func _ready():
+	self.text = level_name
+	connect("button_down", self, "on_button_down")
